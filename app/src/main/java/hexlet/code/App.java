320f155bf71409624808c7e4f3bedb09fd3fcaba
Нуртдинов А. <sogazq@gmail.com>
2026-06-11 09:09:25 +0300

    3rd commit

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 class App {
-public static void main(String[] args) {
+    public static void main(String[] args) {
   System.out.println("Welcome to the Brain Games!");
+  Cli.getName();
+  }
 }
+
+class Cli {
+  public static void getName() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("May I have your name? ");
+    String name = scanner.nextLine();
+    System.out.println("Hello, " + name + "!");
+    scanner.close();
+  }
 }
