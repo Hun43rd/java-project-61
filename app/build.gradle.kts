161288922d521303ad62f555d00e21601c599e91
Нуртдинов А. <sogazq@gmail.com>
2026-06-30 16:29:17 +0300

    CheckStyle added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.github.ben-manes.versions") version "0.54.0"
     application
     id("org.sonarqube") version "7.3.1.8318"
+    checkstyle
 }
 
 group = "hexlet.code"
@@ -32,4 +33,10 @@ sonar {
     property("sonar.projectKey", "Hun43rd_java-project-61")
     property("sonar.organization", "hun43rd")
   }
+}
+checkstyle {
+    toolVersion = "10.12.4"
+    configFile = file("config/checkstyle/checkstyle.xml")
+    isShowViolations = true
+    isIgnoreFailures = false
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 class App {
     public static void main(String[] args) {
-  System.out.println("Welcome to the Brain Games!");
-  Cli.getName();
-  }
+        System.out.println("Welcome to the Brain Games!");
+        Cli.getName();
+    }
 }
 
 class Cli {
-  public static void getName() {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("May I have your name? ");
-    String name = scanner.nextLine();
-    System.out.println("Hello, " + name + "!");
-    scanner.close();
-  }
+    public static void getName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
+    }
 }
