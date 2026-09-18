b801dd82fb8da74acd7fa72c5c98d425cf040bba
Нуртдинов А. <sogazq@gmail.com>
2026-09-18 11:32:19 +0300

    123

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,9 @@
-
 plugins {
     id("com.github.ben-manes.versions") version "0.54.0"
     application
+    id("org.sonarqube") version "7.3.1.8318"
     checkstyle
     id("com.diffplug.spotless") version "8.10.1"
-    id("org.sonarqube") version "7.5.0.8588"
 }
 
 group = "hexlet.code"
@@ -14,31 +13,28 @@ repositories {
     mavenCentral()
 }
 
+tasks.test {
+    useJUnitPlatform()
+}
+
 application {
     mainClass = "hexlet.code.App"
 }
+
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
 }
+
 checkstyle {
     toolVersion = "10.12.4"
     configFile = file("config/checkstyle/checkstyle.xml")
     isShowViolations = true
     isIgnoreFailures = false
 }
-spotless {
-    java {
-        importOrder()
-        removeUnusedImports()
-        googleJavaFormat().aosp()
-        formatAnnotations()
-        leadingTabsToSpaces(4)
-    }
-}
+
 sonar {
     properties {
         property("sonar.projectKey", "Hun43rd_java-project-61")
         property("sonar.organization", "hun43rd")
-        property("sonar.host.url", "https://sonarcloud.io")
     }
 }
