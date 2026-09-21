ba876b3c2c4b71864e476bafb41b271dc15c71ed
Нуртдинов А. <sogazq@gmail.com>
2026-09-21 10:27:43 +0300

    Remove IDE files from repository

--- app/src/main/java/hexlet/code/RandomNumber.java
+++ app/src/main/java/hexlet/code/RandomNumber.java
@@ -6,7 +6,7 @@ class RandomNumber {
     private static final Random RANDOM = new Random();
 
     public static int getNumber() {
-        int number = RANDOM.nextInt(100) + 1;
-        return number;
+        return RANDOM.nextInt(100) + 1;
+        //return number;
     }
 }
